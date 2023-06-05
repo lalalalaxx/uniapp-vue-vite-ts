c5a86259b70c53075f99dd837552037b190b501d
lxx <2399270194@qq.com>
2023-06-05 18:00:21 +0800

    请求的配置

--- src/apis/demo.ts
+++ src/apis/demo.ts
@@ -3,7 +3,7 @@ import { request } from "@/utils/http";
 export function demo() {
     return request.http({
         url: '/index', 
-        data: {aa: 1},
-        isSinglePost: true // 是否是单例模式默认可不传
+        data: {aa: 1},   
+        isSinglePost: true // 是否是单例模式默认可不传de
     })
 } 
